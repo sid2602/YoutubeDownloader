47f02190c648c2d82581bb6fff41a255c1ab9763
Filip Kornaus <kornaus.filip@gmail.com>
2020-06-19 10:00:35 +0200

    initial

---
+++ src/Components/Wrapper/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+
+const Wrapper = styled.main`
+
+    border: 1px solid gray;
+    border-radius: 10px;
+    max-width:1000px;
+    height: 80vh;
+    margin: 100px auto;
+`;
+
+export default Wrapper;
