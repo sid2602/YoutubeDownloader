9b5f0d9d4f28dda8a32279e85c54d1a19de5385c
Filip Kornaus <kornaus.filip@gmail.com>
2020-06-25 13:17:01 +0200

    Updates

---
+++ precache-manifest.a9391fe580e0438cfd8dce9d5f51e7af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "514a9934efb54125b886402d3164e40a",
+    "url": "/YoutubeDownloader/index.html"
+  },
+  {
+    "revision": "927981e019dffe43fa87",
+    "url": "/YoutubeDownloader/static/js/2.2f649d1a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/YoutubeDownloader/static/js/2.2f649d1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc954d01d51cba80b61d",
+    "url": "/YoutubeDownloader/static/js/main.353dec09.chunk.js"
+  },
+  {
+    "revision": "eceb8a503c4e35e14bb7",
+    "url": "/YoutubeDownloader/static/js/runtime-main.15ae9cf2.js"
+  }
+]);
